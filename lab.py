baa6fa0c8b61bb8144b6ee4de7ef7ac6ec0d3962
eric <yom@yomhub.com>
2020-01-26 14:15:55 +0900

    Adding multi feature layers

--- lab.py
+++ lab.py
@@ -19,18 +19,22 @@ if __name__ == "__main__":
 
   # model = Faster_RCNN(num_classes=2)
 
-  vgg16=tf.keras.applications.VGG16(weights='imagenet', include_top=False)
-  feature_model_t = tf.keras.Model(
-        inputs=vgg16.input,
-        outputs=[
-          vgg16.get_layer('block3_pool').output,
-          vgg16.get_layer('block4_conv3').output,
-          vgg16.get_layer('block4_pool').output,
-          vgg16.get_layer('block5_conv3').output
-        ],
-        # name=""
-      )
-  front_feature = feature_model_t(tf.zeros((1,512,512,3),dtype=tf.float32))
-  # model(tf.zeros((1,512,512,3),dtype=tf.float32))
+  # vgg16=tf.keras.applications.VGG16(weights='imagenet', include_top=False)
+  # feature_model_t = tf.keras.Model(
+  #       inputs=vgg16.input,
+  #       outputs=[
+  #         vgg16.get_layer('block3_pool').output,
+  #         vgg16.get_layer('block4_conv3').output,
+  #         vgg16.get_layer('block4_pool').output,
+  #         vgg16.get_layer('block5_conv3').output
+  #       ],
+  #       # name=""
+  #     )
+  # front_feature = feature_model_t(tf.zeros((1,512,512,3),dtype=tf.float32))
+  # # model(tf.zeros((1,512,512,3),dtype=tf.float32))
+  z=tf.zeros((2,2,9))
+  o=tf.ones((2,2,9))
+  rer=tf.stack([z,o],axis=-1)
+  
   print()
   
