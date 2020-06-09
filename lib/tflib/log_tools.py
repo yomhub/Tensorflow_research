5b6815bb28da505b8527f8e52dc582debf46047e
eric <yom@yomhub.com>
2020-06-09 14:35:43 +0900

    Adding plt

--- lib/tflib/log_tools.py
+++ lib/tflib/log_tools.py
@@ -3,6 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+__DEF_LINE_STY = [
+    'solid',    # _____
+    'dotted',   # .......
+    'dashdot',  # __.__.__.
+    'dashed',   # __ __ __
+  ]
+__DEF_COLORS = [
+    'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
+    'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
+    'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
+
 def str2time(instr):
   ymd,hms=instr.split('-')
   return datetime(int(ymd[:4]), int(ymd[4:6]), int(ymd[6:]), int(hms[:2]), int(hms[2:4]), int(hms[4:6]))
@@ -63,29 +74,28 @@ def save_image(img, savedir):
     img = tf.reshape(img,img.shape[1:])
   tf.io.write_file(savedir,tf.io.encode_jpeg(tf.cast(img,tf.uint8)))
 
-def plt_func_lines(funcs,xarry=None,cols=None,fig_size=None,save_name=None):
+def plt_func_lines(funcs,xarry=None,cols=None,figure=None,fig_size=None,save_name=None):
   """
   Draw plt by function
   Args:
     fig_size: width, height
   """
-  plt.figure(figsize=fig_size)
+  fg = plt.figure(figsize=fig_size) if(figure==None)else figure
   if(xarry==None):
     xarry=np.range(-5.0,5.0,100)
   if(type(funcs)!=list):
     funcs = [funcs]
-  tcols = [
-    'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
-    'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
-    'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
-  line_sty = [
-    '','--','-.','--.',':'
-  ]
-  for i in range(len(funcs)):
-    plt.plot(xarry,funcs[i](xarry),tcols[i],linewidth=1.0,linestyle=line_sty[i])
-  plt.savefig(save_name if(save_name!=None)else 'plt_img.jpg')
 
-def plt_points_lines(points,xarry=None,xcfg=None,fig_size=None,save_name=None):
+  for i in range(len(funcs)):
+    fg.plot(
+      xarry,funcs[i](xarry),
+      color=__DEF_COLORS[i%len(__DEF_COLORS)],
+      linewidth=1.0,
+      linestyle=__DEF_LINE_STY[i%len(__DEF_LINE_STY)])
+  if(save_name!=None):fg.savefig(save_name)
+  return fg
+
+def plt_points_lines(points,xarry=None,xcfg=None,figure=None,fig_size=None,save_name=None):
   """
   Draw plt by function
   Args:
@@ -102,16 +112,10 @@ def plt_points_lines(points,xarry=None,xcfg=None,fig_size=None,save_name=None):
       if points is 1D and xarry==None, (start,end) will be use
     
   """
-  plt.figure(figsize=fig_size)
+  fg = plt.figure(figsize=fig_size) if(figure==None)else figure
   if(type(points)!=list):
     points = [points]
-  tcols = [
-    'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds',
-    'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu',
-    'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
-  line_sty = [
-    '','--','-.','--.',':'
-  ]
+
   for i in range(len(points)):
     if(len(points[i].shape)==1):
       if(xarry!=None and xarry.shape[0]==points[i].shape[0]): xs = xarry
@@ -120,8 +124,12 @@ def plt_points_lines(points,xarry=None,xcfg=None,fig_size=None,save_name=None):
     else:
       xs = points[i][1]
       ys = points[i][0]
-    plt.plot(xs,ys,tcols[i%len(tcols)],linewidth=1.0,linestyle=line_sty[i%len(line_sty)])
-  plt.savefig(save_name if(save_name!=None)else 'plt_img.jpg')
+    fg.plot(xs,ys,
+      color=__DEF_COLORS[i%len(__DEF_COLORS)],
+      linewidth=1.0,
+      linestyle=__DEF_LINE_STY[i%len(__DEF_LINE_STY)])
+  if(save_name!=None):fg.savefig(save_name)
+  return fg
 
 def rf_helper(net_list,ord_len,panding=True):
   """
@@ -171,35 +179,64 @@ def visualize_helper(img,gtbox,mask,model):
       module: model with output 
       {}
   """
-  plt.figure(figsize=(8,4))
-  plt.subplot(3,3,3)
+
+  linewidth = 1.3
+  fg = plt.figure(figsize=(8,4))
+  fg.subplot(3,3,3)
   divnum = 3*3-1
+  base_scale = 32 # vgg net based scale
   if(type(img)!=list):img=[img]
   if(type(gtbox)!=list):gtbox=[gtbox]
   if(type(mask)!=list):mask=[mask]
+  for j in range(divnum):
+    fg.subplot(3,3,j+1)
+    fg.xlabel('layer')
+    fg.ylabel('energy')
+    fg.title('Scaler {}/{}'.format(j+1,divnum))
+  dx = None
   for i in range(len(img)):
-    coe_x = int(img.shape[-2]/32)
-    coe_y = int(img.shape[-3]/32)
+    coe_x = int(max(int(img.shape[-2]/base_scale),divnum)/divnum)
+    coe_y = int(max(int(img.shape[-3]/base_scale),divnum)/divnum)
+    
     for j in range(divnum):
-      plt.subplot(3,3,divnum+1)
-      plt.title('Img scaler {}/{}'.format(divnum+1,divnum))
+      img_size = [coe_y*base_scale,coe_x*base_scale]
+      tmp = tf.image.resize(img[i],img_size)
+      rt = model(tmp)
+      mp = tf.cast(rt['scr'][:,:,:,1]>rt['scr'][:,:,:,0],tf.float32)
+      mp = tf.reshape(mp,mp.shape+[1])
+      mp = tf.broadcast_to(mp,mp.shape[:-1]+[3])
+      mp = tf.concat([mp,tf.image.resize(tmp,mp.shape[-3:-1])/255.0],axis=2)
+      tf.summary.image(
+        name = 'Score|Img image size {}.'.format(img_size),
+        data = mp,step=0)
 
-  # size_list = np.linspace(min_h,1.0-max_h,scale_num).reshape((-1,1))*img.shape[-3:-1]
+      if(dx==None):dx=np.arange(len(rt['ftlist']))
+      dmin = []
+      dmean = []
+      dmax = []
+      for o in range(len(rt['ftlist'])):
+        dmin += [tf.reduce_min(rt['ftlist'][o]).numpy()]
+        dmax += [tf.reduce_max(rt['ftlist'][o]).numpy()]
+        dmean += [tf.reduce_mean(rt['ftlist'][o]).numpy()]
 
-  size_list = np.asarray(range(1,min(coe_x,coe_y),1)).reshape((-1,1))*[32,32]
-  # size_list = np.linspace(1,coe_x,scale_num).reshape((-1,1))*[32,32]
-  for i in range(len(size_list)):
-    tmp = tf.image.resize(img,size_list[i])
-    rt = model(tmp)
-    mp = tf.cast(rt['scr'][:,:,:,1]>rt['scr'][:,:,:,0],tf.float32)
-    mp = tf.reshape(mp,mp.shape+[1])
-    mp = tf.broadcast_to(mp,mp.shape[:-1]+[3])
-    mp = tf.concat([mp,tf.image.resize(tmp,mp.shape[-3:-1])/255.0],axis=2)
-    tf.summary.image(
-      name = 'Score|Img image size {}.'.format(size_list[i]),
-      data = mp,step=0)
+      dmin = np.asarray(dmin)
+      dmean = np.asarray(dmean)
+      dmax = np.asarray(dmax)
 
-    for o in range(len(rt['ftlist'])):
-      tf.summary.scalar('Min/image size {}.'.format(size_list[i]),tf.reduce_min(rt['ftlist'][o]),step=o)
-      tf.summary.scalar('Max/image size {}.'.format(size_list[i]),tf.reduce_max(rt['ftlist'][o]),step=o)
-      tf.summary.scalar('Mean/image size {}.'.format(size_list[i]),tf.reduce_mean(rt['ftlist'][o]),step=o)
+      fg.subplot(3,3,j+1)
+      fg.plot(dx,dmean,
+        color=__DEF_COLORS[i%len(__DEF_COLORS)],
+        linewidth=linewidth,
+        linestyle=__DEF_LINE_STY[0],
+        label='mean')
+      fg.plot(dx,dmin,
+        color=__DEF_COLORS[i%len(__DEF_COLORS)],
+        linewidth=linewidth,
+        linestyle=__DEF_LINE_STY[1],
+        label='min')
+      fg.plot(dx,dmax,
+        color=__DEF_COLORS[i%len(__DEF_COLORS)],
+        linewidth=linewidth,
+        linestyle=__DEF_LINE_STY[2],
+        label='max')
+fg.savefig('logfig.jpg')
